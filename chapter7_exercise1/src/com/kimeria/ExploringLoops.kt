e99e3faeb74896d01730005b68d43391e6adf95c
wkimeria <wkimeria@gmail>
2022-01-21 16:18:52 -0800

    After chapter 9: Ranges and Looping

---
+++ chapter7_exercise1/src/com/kimeria/ExploringLoops.kt
@@ -0,0 +1,59 @@
+package com.kimeria
+
+import kotlin.math.pow
+
+fun main() {
+    /*
+    var x: Int = 0
+
+    while(x < 10){
+        println(x)
+        x++
+    }
+     */
+
+    //forLoop()
+    //forLoopKeyValue()
+    rangeLoop()
+}
+
+fun rangeLoop(){
+    for( i in 0..9){
+        println(i)
+    }
+    (0..9).forEach{println(it.toFloat().pow(it))}
+    (9 downTo 0).forEach{println(it.toFloat().pow(it))}
+    (0 until 9).forEach{println(it)}
+    (0 ..9 step 3).forEach{println(it)}
+    ('A' ..'F').forEach{println(it)}
+}
+
+fun forLoopKeyValue(){
+    val people = HashMap<Int, PersonKotlin>()
+    people[1] = PersonKotlin(1, "Mr" ,"John","Doe")
+    people[2] = PersonKotlin(2, "Mrs" ,"Jane","Doe")
+    people[3] = PersonKotlin(3, "Ms" ,"Sally","Green")
+
+    for((key, value) in people){
+        println("key=$key value=$value")
+    }
+
+}
+fun forLoop(){
+    val people = ArrayList<PersonKotlin>()
+    people.add(PersonKotlin(1, "Mr" ,"John","Doe"))
+    people.add(PersonKotlin(2, "Mrs" ,"Jane","Doe"))
+    people.add(PersonKotlin(3, "Ms" ,"Sally","Green"))
+
+    for(person in people){
+        println(person)
+        val (id, title) = person
+        println("$id $title")
+    }
+
+    for((id, title, firstName, surName) in people){
+        println("$id $title $firstName $surName")
+
+    }
+}
+
--- chapter7_exercise1/src/com/kimeria/exercise1.kt
+++ chapter7_exercise1/src/com/kimeria/exercise1.kt
@@ -6,41 +6,72 @@ import java.util.*
 fun main() {
     val john = PersonKotlin(11L, "Mr", "John", "Blue",
         GregorianCalendar(1977, Calendar.SEPTEMBER, 3))
+    val john2 = PersonKotlin(11L, "Mr", "John", "Blue",
+        GregorianCalendar(1977, Calendar.SEPTEMBER, 3))
     val jane = PersonKotlin(21L, "Mrs", "Jane", "Green", null)
     println("${john}'s age is ${john.age}")
     println("${jane}'s age is ${jane.age}")
     println("The age of someone born on 3rd May 1988 is " + PersonKotlin.age(GregorianCalendar(1988, Calendar.MAY, 3)))
+
+    println(john)
+    println(jane)
+    println("Object Equality ${john == john2}")
+    println("Reference Equality ${john === john2}")
+
+    john.favoriteColor = "red"
+    println(john.getColorType())
+    john.favoriteColor = "orange"
+    println(john.getColorType())
 }
 
 
 data class PersonKotlin(val id: Long, val title: String, val firstName: String, val surname: String,
                         val dateOfBirth: Calendar? = GregorianCalendar(1977, Calendar.SEPTEMBER, 3)){
 
-    val age: Int
-    get(){
-        if (dateOfBirth == null){
-          return -1
+    var favoriteColor: String? = null
+
+    fun getColorType(): String {
+        return when (getUpperCaseColor()) {
+            "" -> "empty"
+            "RED", "BLUE", "GREEN" -> "rgb"
+            else -> "other"
         }
-        return age(dateOfBirth)
+
     }
+    fun getLastLetter(a: String) = a.takeLast(1)
+
+    fun getLastLetterOfColor(): String {
+        return favoriteColor?.let{ getLastLetter(it)} ?:""
+    }
+
+    fun getUpperCaseColor(): String?{
+        return favoriteColor?.uppercase() ?:""
+    }
+
+    val safeAge: Int
+    get() = age ?: -1 //Elvis Operator
+
+    val age: Int?
+    get() = if (dateOfBirth == null) -1 else age(dateOfBirth)
+
+
+
 
     override fun toString(): String {
         val fmt = SimpleDateFormat("dd-MMM-yyyy")
         if(dateOfBirth == null){
-            return "$id $title $firstName $surname no birth date"
+            return "id=$id title=$title firstName=$firstName surname=$surname dob=None age=$safeAge"
         }
-        return "$id $title $firstName $surname ${fmt.format(dateOfBirth.time)}"
+        return "id=$id title=$title firstName=$firstName surname=$surname dob=${fmt.format(dateOfBirth.time)} age=$safeAge"
     }
 
     companion object {
-        fun age(dateOfBirth: Calendar): Int {
+        fun age(dateOfBirth: Calendar?): Int? {
+            if (dateOfBirth == null) return null
+
             val today = GregorianCalendar()
             val years: Int = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR)
-            return if (dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)) {
-                years - 1
-            } else {
-                years
-            }
+            return if(dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR))years - 1 else years
         }
     }
 }
